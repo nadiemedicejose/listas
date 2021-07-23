8057ab8e147c9452258b2e5653319abcee8bf9a4
Jose Estrada <j.estrada49@icloud.com>
2021-07-22 19:23:19 -0700

    Creación del arreglo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,6 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  // Crear variable tipo array
+  lista: Array<any> = [
+    {titulo: 'Ingeniería Ambiental'},
+    {titulo: 'Ingeniería de Procesos'},
+    {titulo: 'Ingeniería Biomédica'},
+    {titulo: 'Ingeniería Biotecnología Acuática'},
+    {titulo: 'Ingeniería en Geociencias'},
+    {titulo: 'Ingeniería en Horticultura'},
+    {titulo: 'Ingeniería Industrial en Manufactura'},
+    {titulo: 'Ingeniería en Logística y Transporte'},
+    {titulo: 'Ingeniería en Mecatrónica'},
+    {titulo: 'Ingeniería en Software'},
+    {titulo: 'Ingeniería en Tecnología de Alimentos'}
+  ]
 
   constructor() {}
 
