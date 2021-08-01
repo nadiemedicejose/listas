9ee89004fe3c8c43e579e774854ed3d044cdb4ff
Jose Estrada <j.estrada49@icloud.com>
2021-07-31 18:33:06 -0700

    Aplicación terminada

--- src/app/directorio/directorio.page.ts
+++ src/app/directorio/directorio.page.ts
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./directorio.page.scss'],
 })
 export class DirectorioPage implements OnInit {
+  // Crear variable tipo array
+  lista: Array<any> = [
+    {
+      titulo: 'Rector',
+      nombre: 'DR. PEDRO ORTEGA ROMERO',
+      imagen: 'assets/images/portega.jpg'
+    },
+    {
+      titulo: 'Secretario Técnico',
+      nombre: 'LIC. RICARDO BARRÓN MORALES',
+      imagen: 'assets/images/rbarron.jpg'
+    },
+    {
+      titulo: 'Coordinadora del Área Jurídica',
+      nombre: 'LIC. ANA SILVIA VALENZUELA RUIZ',
+      imagen: 'assets/images/avlzla.jpg'
+    }
+  ]
 
   constructor() { }
 
